d9af088a7e243f42cea8c674d5748ea880176ea6
Adan <programmerAdan@gmail.com>
2019-07-08 13:04:29 -0500

    Writing up Code for TankPlayerController

--- TankProject/Source/TankProject/TankPlayerController.cpp
+++ TankProject/Source/TankProject/TankPlayerController.cpp
@@ -2,6 +2,25 @@
 
 #include "TankPlayerController.h"
 
+void ATankPlayerController::BeginPlay()
+{
+	Super::BeginPlay();
+
+	auto ControlledTank = GetControlledTank();
+
+	if (!ControlledTank)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("PlayerController not possessing a tank"));
+	}
+	else
+	{
+		UE_LOG(LogTemp, Warning, TEXT("PlayerController possessing: %s"), *(ControlledTank->GetName()));
+	}
 
 
+}
 
+ATank * ATankPlayerController::GetControlledTank()
+{
+	return Cast<ATank>(GetPawn());
+}
