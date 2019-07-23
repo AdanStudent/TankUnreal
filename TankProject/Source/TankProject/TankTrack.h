0a3ac78b5451365901c333b48a4240b5c1b1abf7
Adan <programmerAdan@gmail.com>
2019-07-23 12:56:53 -0500

    Setup Throttle for Tracks

--- TankProject/Source/TankProject/TankTrack.h
+++ TankProject/Source/TankProject/TankTrack.h
@@ -17,5 +17,9 @@ class TANKPROJECT_API UTankTrack : public UStaticMeshComponent
 public:
 	UFUNCTION(BlueprintCallable, Category = Input)
 	void SetThrottle(float Throttle);
+
+	//Max force per track in Newtons
+	UPROPERTY(EditDefaultsOnly)
+		float TrackMaxDrivingForce = 400000;
 	
 };
