8f560c8abbd719792f90e003d0528aab6fe77be8
Adan <programmerAdan@gmail.com>
2019-07-23 13:17:56 -0500

    Creating Physics Material
    Got Tank moving

--- TankProject/Source/TankProject/TankTrack.h
+++ TankProject/Source/TankProject/TankTrack.h
@@ -20,6 +20,6 @@ public:
 
 	//Max force per track in Newtons
 	UPROPERTY(EditDefaultsOnly)
-		float TrackMaxDrivingForce = 400000;
+		float TrackMaxDrivingForce = 40000000; //40 million Newtons
 	
 };
